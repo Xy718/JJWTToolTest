fce6ae555132bbe49156aa6a154c1dc540387323
Xy718 <869839000@qq.com>
2019-12-16 22:34:52 +0800

    init

---
+++ src/main/java/xyz/xy718/JjwtToolTestApplication.java
@@ -0,0 +1,13 @@
+package xyz.xy718;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjwtToolTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjwtToolTestApplication.class, args);
+	}
+
+}
